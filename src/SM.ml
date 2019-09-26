53cd186419cff43855942708056c0c67fe3da46f
Your Name <you@example.com>
2019-09-26 22:02:48 +0300

    hw done

--- src/SM.ml
+++ src/SM.ml
@@ -1,16 +1,16 @@
-open GT       
+open GT
 open Language
-       
+
 (* The type for the stack machine instructions *)
 @type insn =
 (* binary operator                 *) | BINOP of string
-(* put a constant on the stack     *) | CONST of int                 
+(* put a constant on the stack     *) | CONST of int
 (* read to stack                   *) | READ
 (* write from stack                *) | WRITE
 (* load a variable to the stack    *) | LD    of string
 (* store a variable from the stack *) | ST    of string with show
 
-(* The type for the stack machine program *)                                                               
+(* The type for the stack machine program *)
 type prg = insn list
 
 (* The type for the stack machine configuration: a stack and a configuration from statement
@@ -23,8 +23,35 @@ type config = int list * Stmt.config
      val eval : config -> prg -> config
 
    Takes a configuration and a program, and returns a configuration as a result
- *)                         
-let eval _ = failwith "Not yet implemented"
+ *)
+let eval_step (st, conf) step =
+  match step with
+  | BINOP op ->
+    let x :: y :: tail = st in
+    let res = Language.Expr.eval_one op x y in
+    (res :: tail, conf)
+  | CONST c ->
+    (c :: st, conf)
+  | READ ->
+    let (s, i, o) = conf in
+    let z :: rest = i in
+    (z :: st, (s, rest, o))
+  | WRITE ->
+    let (s, i, o) = conf in
+    let z :: rest = st in
+    (rest, (s, i, o @ [z]))
+  | LD x ->
+    let (s, _, _) = conf in
+    (s x :: st, conf)
+  | ST x ->
+    let (s, i, o) = conf in
+    let z :: rest = st in
+    (rest, (Language.Expr.update x z s, i, o))
+
+let rec eval c p =
+  match p with
+  | [] -> c
+  | step :: rest -> eval (eval_step c step) rest
 
 (* Top-level evaluation
 
@@ -41,4 +68,15 @@ let run p i = let (_, (_, _, o)) = eval ([], (Language.Expr.empty, i, [])) p in 
    Takes a program in the source language and returns an equivalent program for the
    stack machine
  *)
-let compile _ = failwith "Not yet implemented"
+let rec compile stmt =
+  let rec compile_expr expr =
+    match expr with
+    | Language.Expr.Const c -> [CONST c]
+    | Language.Expr.Var x -> [LD x]
+    | Language.Expr.Binop (op, e1, e2) -> compile_expr e2 @ compile_expr e1 @ [BINOP op]
+  in
+  match stmt with
+  | Language.Stmt.Read x -> [READ; ST x]
+  | Language.Stmt.Write e -> compile_expr e @ [WRITE]
+  | Language.Stmt.Assign (x, e) -> compile_expr e @ [ST x]
+  | Language.Stmt.Seq (s1, s2) -> compile s1 @ compile s2
